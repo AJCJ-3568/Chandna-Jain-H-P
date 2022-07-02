ecec7999b92e9f09b4a2cce8e4cec93b48582704
AJCJ-3568 <84306031+AJCJ-3568@users.noreply.github.com>
2022-07-02 21:20:52 +0530

    spelling correction is done

--- index.php
+++ index.php
@@ -21,18 +21,19 @@
     <div class='container'>
       <div>
         <div class='row '>
-          <div class=' col-lg-6'>
+          <div class=' col-sm-12'>
             <h1 class='cont-h1 m-5 col-sm-12' style="font-size:5vw;">Hey friends! </h1>
           </div>
         </div>
         <div class='row'>
-          <div class=' col-lg-6 col-sm-12'>
-            <h3 class='content ' style="font-size:3vw;"> I'm a designer and full stack web developer 🧑‍💻
+          <div class='  col-sm-6'>
+            <h3 class='content ' style="font-size:3vw;">
+              I'm a designer and full stack web developer 🧑‍💻
               built attractive digital experience website,
-              I designer cool stuff for the web </h3>
+              I Designs cool stuff for the web </h3>
           </div>
-          <div class='  col-lg-6 '>
-            <img class="cjImg rounded float-left img-responsive col-sm-12" style="width:100%;  height: auto;" src="./assets/img/cjwebsite.webp" alt="CJ" />
+          <div class='col-sm-6'>
+            <img class="cjImg rounded float-left img-responsive col-sm-8" style="width:100%;  height: auto;" src="./assets/img/cjwebsite.webp" alt="CJ" />
           </div>
 
         </div>
