060f292d00ed418224ba25b592117859ce26809a
AJCJ-3568 <84306031+AJCJ-3568@users.noreply.github.com>
2022-07-17 17:49:12 +0530

    add nav computere science

--- commons/navigation.php
+++ commons/navigation.php
@@ -8,12 +8,15 @@
             <ul class="navbar-nav ">
                 <li class="nav-item ">
                     <a class="nav-link " href="./index.php">HOME</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link " href="./BLOG.php">BLOG</a>
+                    <li class="nav-item">
+                        <a class="nav-link " href="./BLOG.php">BLOG</a>
+                    </li>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link " href="./WORK.php">WORK</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="https://developing-wren-cea.notion.site/Computer-Science-4bb05ff0318540a98500e47db56c4090">Computer Science</a>
                 </li>
             </ul>
     </div>
